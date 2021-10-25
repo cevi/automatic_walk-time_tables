535fc28be997a339b969e5726e72b93b58682982
Cyrill Püntener <wp99cp@gmx.net>
2021-10-25 19:37:48 +0200

    added multi map export with custom scale #8
    - first version of code, cleanup and optimizations needed!

--- python_program/automatic_walk_time_tables/map_downloader/create_map.py
+++ python_program/automatic_walk_time_tables/map_downloader/create_map.py
@@ -1,9 +1,13 @@
-import json
-import time
-from pathlib import Path
-
 import gpxpy
+import json
+import matplotlib.pyplot as plt
+import numpy as np
 import requests
+import time
+from matplotlib.patches import Rectangle
+from pathlib import Path
+from pyclustering.cluster.kmeans import kmeans, kmeans_visualizer
+from pyclustering.utils.metric import type_metric, distance_metric
 
 from .. import coord_transformation
 
@@ -31,36 +35,45 @@ def plot_route_on_map(raw_gpx_data: gpxpy.gpx,
 
     """
 
-    query_json = create_mapfish_query(layer, map_scaling, raw_gpx_data)
+    map_centers = create_map_centers(map_scaling, raw_gpx_data)
 
-    base_url = "{}://{}:{}".format(print_api_protocol, print_api_base_url, print_api_port)
-    url = '{}/print/default/report.pdf'.format(base_url)
-    response_obj = requests.post(url, data=json.dumps(query_json))
 
-    if response_obj.status_code != 200:
-        raise Exception('Can not fetch map. Status Code: {}'.format(response_obj.status_code))
+    if len(map_centers) > 10:
+        raise Exception("You should respect the faire use limit!")
+        return
 
-    response_json = json.loads(response_obj.content)
+    for index, map_center in enumerate(map_centers):
 
-    pdf_status = requests.get(base_url + response_json['statusURL'])
-    while pdf_status.status_code == 200 and json.loads(pdf_status.content)['status'] == 'running':
-        time.sleep(0.5)
+        query_json = create_mapfish_query(layer, map_scaling, raw_gpx_data, map_center)
+
+        base_url = "{}://{}:{}".format(print_api_protocol, print_api_base_url, print_api_port)
+        url = '{}/print/default/report.pdf'.format(base_url)
+        response_obj = requests.post(url, data=json.dumps(query_json))
+
+        if response_obj.status_code != 200:
+            raise Exception('Can not fetch map. Status Code: {}'.format(response_obj.status_code))
+
+        response_json = json.loads(response_obj.content)
+
         pdf_status = requests.get(base_url + response_json['statusURL'])
-        print(json.loads(pdf_status.content)['status'])
+        while pdf_status.status_code == 200 and json.loads(pdf_status.content)['status'] == 'running':
+            time.sleep(0.5)
+            pdf_status = requests.get(base_url + response_json['statusURL'])
+            print(json.loads(pdf_status.content)['status'])
 
-    if response_obj.status_code != 200 and json.loads(pdf_status.content)['status'] != 'finished':
-        raise Exception('Can not fetch map. Status Code: {}'.format(response_obj.status_code))
+        if response_obj.status_code != 200 and json.loads(pdf_status.content)['status'] != 'finished':
+            raise Exception('Can not fetch map. Status Code: {}'.format(response_obj.status_code))
 
-    fetched_pdf = requests.get(base_url + response_json['downloadURL'])
+        fetched_pdf = requests.get(base_url + response_json['downloadURL'])
 
-    if response_obj.status_code != 200:
-        raise Exception('Can not fetch map. Status Code: {}'.format(response_obj.status_code))
+        if response_obj.status_code != 200:
+            raise Exception('Can not fetch map. Status Code: {}'.format(response_obj.status_code))
 
-    with open('output/{}_map.pdf'.format(file_name), 'wb') as f:
-        f.write(fetched_pdf.content)
+        with open('output/{}_{}_map.pdf'.format(file_name, index), 'wb') as f:
+            f.write(fetched_pdf.content)
 
 
-def create_mapfish_query(layer, map_scaling, raw_gpx_data):
+def create_mapfish_query(layer, map_scaling, raw_gpx_data, center):
     """
 
     Returns the JSON-Object used for querying
@@ -78,7 +91,7 @@ def create_mapfish_query(layer, map_scaling, raw_gpx_data):
         "outputFormat": "pdf",
         "attributes": {
             "map": {
-                "center": path_coordinates[0],
+                "center": center,
                 "scale": map_scaling,
                 "dpi": 400,
                 "pdfA": True,
@@ -144,7 +157,75 @@ def create_mapfish_query(layer, map_scaling, raw_gpx_data):
             }
         }
     }
+
+
     return query_json
+
+
+def GetSpacedElements(array, numElems=4):
+
+    """
+    Get numElems of an array, that are equally spaced based on index (not value).
+    """
+
+    indices = np.round(np.linspace(0, len(array) - 1, numElems)).astype(int)
+    return list(np.array(array)[indices])
+
+
+def create_map_centers(map_scaling: int, raw_gpx_data: gpxpy.gpx) -> []:
+    """
+
+    Calculates the map centers based on the approach discussed here:
+    https://stackoverflow.com/questions/51946065/cover-a-polygonal-line-using-the-least-given-rectangles-while-keeping-her-contin
+
+    """
+
+    points = get_path_coordinates_as_list(raw_gpx_data)
+
+    w = 6.5 / 25.0 * map_scaling
+    h = 4.5 / 25.0 * map_scaling
+    n = 0
+    n_points = 200
+
+    path_covered = False
+
+    while not path_covered:
+
+        n = n + 1
+        points_for_clustering = GetSpacedElements(points, n_points)
+
+        user_function = lambda point1, point2: max(abs(point1[0] - point2[0]) / (w / 2),
+                                                   abs(point1[1] - point2[1]) / (h / 2))
+        metric = distance_metric(type_metric.USER_DEFINED, func=user_function)
+
+        # create K-Means algorithm with specific distance metric
+        start_centers = GetSpacedElements(points_for_clustering, n)
+        kmeans_instance = kmeans(points_for_clustering, start_centers, metric=metric)
+
+        # Run cluster analysis and obtain results.
+        kmeans_instance.process()
+        clusters = kmeans_instance.get_clusters()
+        final_centers = kmeans_instance.get_centers()
+
+        path_covered = True
+
+        for i, pkt in enumerate(points):
+
+            point_covered = False
+            for center in final_centers:
+                d = user_function(center, pkt)
+                if d < 1:
+                    point_covered = True
+                    break
+
+            if not point_covered:
+                path_covered = False
+                break
+
+        if n >= 25:
+            path_covered = True
+
+    return final_centers
 
 
 def get_path_coordinates_as_list(raw_gpx_data):
