cevi/automatic_walk-time_tables

commit 92831b3fb05571714ccfac861fe939b2187519fb

change default output directory
* set the default output directory to `./output`currently it was `./`
* move the creation of the output directory from `app.py`to the `automatic_walk_time_table_generator.py` such that the command line argument also works in a no-docker environment.
* update documentation

python_program/app.py
@@ -39,9 +39,7 @@ def create_map():
     logger.debug('New request to with create_map().', {'uuid': uuid})
 
     # check if output and input folders exist, if not, create them
-    output_directory = 'output/' + uuid + '/'
     input_directory = 'input/' + uuid + '/'
-    pathlib.Path(output_directory).mkdir(parents=True, exist_ok=True)
     pathlib.Path(input_directory).mkdir(parents=True, exist_ok=True)
 
     if 'file' not in request.files:

python_program/arg_parser.py
@@ -32,7 +32,7 @@ def create_parser():
                         help='Departure date in ISO-format, i.g. 2011-11-04T00:05:23. Default 2021-08-16T09:00:00.')
     parser.add_argument('-n', '--creator-name', type=str, metavar='<name>',
                         help='Name of you, the creator of this route. If not specified, it will be empty.', default='')
-    parser.add_argument('--output_directory', default='', type=str,
+    parser.add_argument('--output_directory', default='./output/', type=str,
                         help='Subdirectory in the output folder for storing the created files. Should be empty or ending with "/"')
     parser.add_argument('--log-level', type=int, metavar='<log-level>', default=30,
                         help='Log Level (see https://docs.python.org/3/library/logging.html#levels). Default: WARNING.')

python_program/automatic_walk_time_tables/automatic_walk_time_table_generator.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import pathlib
 
 import gpxpy
 
@@ -26,6 +27,7 @@ class AutomatedWalkTableGenerator:
         self.raw_gpx_data = gpxpy.parse(gpx_file)
 
         self.output_directory = args.output_directory
+        pathlib.Path(self.output_directory).mkdir(parents=True, exist_ok=True)
 
     def run(self):
         name = self.output_directory + self.raw_gpx_data.name

commit 2ce81a0c11df290267b7995025e763bb0363bf1b

fix bug introduced with 92831b3fb05571714ccfac861fe939b2187519fb
*

python_program/app.py
@@ -40,6 +40,7 @@ def create_map():
 
     # check if output and input folders exist, if not, create them
     input_directory = 'input/' + uuid + '/'
+    output_directory = 'output/' + uuid + '/'
     pathlib.Path(input_directory).mkdir(parents=True, exist_ok=True)
 
     if 'file' not in request.files: