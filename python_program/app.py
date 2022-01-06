cevi/automatic_walk-time_tables

commit 4793f7619b1a615bee40e739efc8ee3aae82aa2e

fix 505 if you request an in-existent document
* remove uuid from stateHandler after download once the data gets deleted
* check if the folder containing the data exists, if not return an error message

python_program/app.py
@@ -5,9 +5,9 @@ import json
 import logging
 import os
 import pathlib
+import shutil
 import uuid as uuid_factory
 import zipfile
-import shutil
 from threading import Thread
 
 import flask
@@ -121,7 +121,12 @@ def return_status(uuid):
 
 @app.route('/download/<uuid>')
 def request_zip(uuid):
+    # Check if export is completed and still present in the 'output' folder
     base_path = pathlib.Path('./output/' + uuid + '/')
+    state = stateHandler.get_status(uuid)
+
+    if (state and state != 'finished') or not os.path.exists(base_path):
+        return "Die angefragten Daten sind nicht (mehr) verfügbar."
 
     # Return Zip with data
     data = io.BytesIO()
@@ -134,6 +139,7 @@ def request_zip(uuid):
 
     # delete and return files
     try:
+        stateHandler.remove_status(uuid)
         shutil.rmtree(base_path)
     except OSError as e:
         logger.error("Cannot delete files in folder %s : %s" % (base_path, e.strerror))

commit 879c29f4fc31f704bfb3029ffb2be98cc18ae16b

fixes bug introduced with 4793f7619b1a615bee40e739efc8ee3aae82aa2e
* download not available
* thanks @maede97  for pointing it out

python_program/app.py
@@ -125,7 +125,6 @@ def request_zip(uuid):
     base_path = pathlib.Path('./output/' + uuid + '/')
     state = stateHandler.get_status(uuid)
 
-    if (state and state != 'finished') or not os.path.exists(base_path):
         return "Die angefragten Daten sind nicht (mehr) verfügbar."
 
     # Return Zip with data