cevi/automatic_walk-time_tables

commit 879c29f4fc31f704bfb3029ffb2be98cc18ae16b

fixes bug introduced with 4793f7619b1a615bee40e739efc8ee3aae82aa2e
* download not available
* thanks @maede97  for pointing it out

python_program/app.py
@@ -125,7 +125,6 @@ def request_zip(uuid):
     base_path = pathlib.Path('./output/' + uuid + '/')
     state = stateHandler.get_status(uuid)
 
-    if (state and state != 'finished') or not os.path.exists(base_path):
         return "Die angefragten Daten sind nicht (mehr) verfügbar."
 
     # Return Zip with data

commit 178074c1af44d5370f1df8a2c3ec7b901af7796e

fixes bug introduced with 879c29f4fc31f704bfb3029ffb2be98cc18ae16b
* download not available

python_program/app.py
@@ -125,6 +125,7 @@ def request_zip(uuid):
     base_path = pathlib.Path('./output/' + uuid + '/')
     state = stateHandler.get_status(uuid)
 
+    if (state and state['status'] != 'finished') or not os.path.exists(base_path):
         return "Die angefragten Daten sind nicht (mehr) verfügbar."
 
     # Return Zip with data