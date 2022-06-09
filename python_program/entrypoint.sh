94a407d862f4e934eb648072bb799ca4c0a67deb
Matthias Busenhart <busenham@student.ethz.ch>
2022-06-09 18:57:26 +0200

    Fix dockerfiles to also run on windows

--- python_program/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Start Backend Server
-echo "Start Backend Server"
-exec gunicorn --bind :5000 --workers 1 --threads 2 --timeout 60 app:app
--- swiss_TLM_api/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Start Backend Server
-echo "Start Backend Server"
-exec gunicorn --bind :1848 --workers 1 --threads 1 --timeout 60 app:app
