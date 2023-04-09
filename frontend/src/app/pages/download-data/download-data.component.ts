8f739204a340e821097506b393aa37e7a20c3167
Cyrill Püntener <wp99cp@gmx.net>
2023-04-09 21:12:49 +0200

    add a download buttom #82 and #158

--- frontend/src/app/pages/download-data/download-data.component.ts
+++ frontend/src/app/pages/download-data/download-data.component.ts
@@ -12,14 +12,24 @@ export class DownloadDataComponent implements OnInit {
 
   static baseURL = environment.API_URL;
   private readonly uuid: string;
+  public readonly download_link: string;
+
+  public readonly available_until: string;
 
   constructor(activatedRoute: ActivatedRoute, public statusManager: StatusManagerService) {
     // Get UUID from the URL
     this.uuid = activatedRoute.snapshot.url[1].toString();
+    this.download_link = DownloadDataComponent.baseURL + 'download/' + this.uuid;
+
+    // the data is available for 10 minutes
+    const available_until = new Date();
+    available_until.setMinutes(available_until.getMinutes() + 5);
+    this.available_until = available_until.toLocaleString();
+
   }
 
   ngOnInit(): void {
-    window.open(DownloadDataComponent.baseURL + 'download/' + this.uuid, '_blank');
+    window.open(this.download_link, '_blank');
   }
 
 }
