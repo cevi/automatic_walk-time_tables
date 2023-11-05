e59d6282a66c775b08b7b7faef0fdc5971e6cff1
maede97 <maede97@hotmail.com>
2023-11-05 17:31:15 +0100

    fix linting again

--- backend/app.py
+++ backend/app.py
@@ -233,7 +233,6 @@ def create_map():
         options = request.get_json()
     elif "options" in request.form:
         options = json.loads(request.form["options"])
-        
     else:
         return app.response_class(
             response=json.dumps(
