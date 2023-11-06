f9d796ecb6fdd4fe8a6940f68b4123b14decfbc5
Cyrill Püntener <wp99cp@gmx.net>
2023-11-06 13:54:14 +0100

    update pypdf to latest version

--- backend/automatic_walk_time_tables/map_downloader/create_map.py
+++ backend/automatic_walk_time_tables/map_downloader/create_map.py
@@ -8,7 +8,7 @@ from typing import List
 
 import numpy as np
 import requests
-from PyPDF2 import PdfFileReader, PdfFileMerger
+from pypdf import PdfWriter
 from pyclustering.cluster.kmeans import kmeans
 from pyclustering.utils.metric import type_metric, distance_metric
 
@@ -230,14 +230,16 @@ class MapCreator:
             self.logger.info("Saved map to {}_{}_map.pdf".format(file_name, index))
 
         # combine the PDFs into a single file
-        merger = PdfFileMerger()
+        merger = PdfWriter()
         for index, map_center in enumerate(map_centers):
-            merger.append(PdfFileReader("{}_{}_map.pdf".format(file_name, index), "rb"))
+            merger.append("{}_{}_map.pdf".format(file_name, index))
             os.remove("{}_{}_map.pdf".format(file_name, index))
 
         merger.write("{}_maps.pdf".format(file_name))
+        merger.close()
 
     def create_mapfish_query(
+
         self, map_layers, map_scaling, center, way_points: path.Path, pois: path.Path
     ):
         """
