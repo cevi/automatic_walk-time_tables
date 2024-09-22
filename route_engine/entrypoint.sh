3d9979bb0c991b453bdd385864e0f89aca380157
Matthias Hüppi <maede97@hotmail.com>
2024-09-22 12:11:50 +0200

    update entrypoint

--- route_engine/entrypoint.sh
+++ route_engine/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # check if /custom_files contains some files apart from .gitkeep
-if ! [ "$(ls -A /custom_files | grep -Ev '.gitkeep' | file_hashes.txt)" ]; then
+if ! [ "$(ls -A /custom_files | grep -Ev '.gitkeep')" ]; then
 
   echo "No custom files found in /custom_files. Start downloading default files..."
 
   # Install gdown
-  pip install gdown --break-system-packages
+  pip install gdown
   export PATH=$PATH:/home/valhalla/.local/bin
 
   # clear /custom_files
   sudo rm -rf /custom_files/*
   sudo chmod 777 /custom_files
 
-  gdown --id 1lkrS268Mphtifxmdw0UHVnR9_cNErNks -O /custom_files/
+  gdown 1lkrS268Mphtifxmdw0UHVnR9_cNErNks -O /custom_files/
 
   echo "Default files downloaded successfully"
 
