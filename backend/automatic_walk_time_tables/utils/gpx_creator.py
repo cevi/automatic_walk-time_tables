f1e4ad00b467d363a3f1381b09ba291374cb9116
Matthias Hüppi <maede97@hotmail.com>
2024-09-22 15:28:38 +0200

    add names to points

--- backend/automatic_walk_time_tables/utils/gpx_creator.py
+++ backend/automatic_walk_time_tables/utils/gpx_creator.py
@@ -18,8 +18,9 @@ def create_gpx_file(path: path.Path, pois: path.Path):
         lat = p84.lat
         lon = p84.lon
         elevation = p84.h
+        name = point.name
         gpx_segment.points.append(
-            gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=elevation)
+            gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=elevation, name=name)
         )
 
     gpx_track.segments.append(gpx_segment)
@@ -29,6 +30,11 @@ def create_gpx_file(path: path.Path, pois: path.Path):
         p84 = point.point.to_WGS84()
         lat = p84.lat
         lon = p84.lon
-        gpx_f.waypoints.append(gpxpy.gpx.GPXWaypoint(lat, lon))
+        elevation = p84.h
+        name = point.name
+        
+        wp = gpxpy.gpx.GPXWaypoint(lat, lon, elevation=elevation, name=name)
+        gpx_f.waypoints.append(wp)
+
 
     return gpx_f.to_xml()
