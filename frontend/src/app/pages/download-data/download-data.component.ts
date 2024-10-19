1944d1a8aa064052769123ee4fabf0607a891861
Matthias Hüppi <maede97@hotmail.com>
2024-10-19 12:29:54 +0200

    add retrieve link to frontend

--- frontend/src/app/pages/download-data/download-data.component.ts
+++ frontend/src/app/pages/download-data/download-data.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {ActivatedRoute} from "@angular/router";
 import {StatusManagerService} from "../../services/status-manager.service";
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-download-data',
@@ -13,14 +15,16 @@ export class DownloadDataComponent implements OnInit {
   static baseURL = environment.API_URL;
   private readonly uuid: string;
   public readonly download_link: string;
+  public readonly retrieve_link: string;
   public readonly qr_link: string;
 
   public readonly available_until: string;
 
-  constructor(activatedRoute: ActivatedRoute, public statusManager: StatusManagerService) {
+  constructor(activatedRoute: ActivatedRoute, public statusManager: StatusManagerService, private clipboard: Clipboard, private snackBar: MatSnackBar) {
     // Get UUID from the URL
     this.uuid = activatedRoute.snapshot.url[1].toString();
     this.download_link = DownloadDataComponent.baseURL + 'download/' + this.uuid;
+    this.retrieve_link = DownloadDataComponent.baseURL + 'retrieve/' + this.uuid;
     this.qr_link = DownloadDataComponent.baseURL + 'qr/' + this.uuid;
 
     // the data is available for 10 minutes
@@ -32,6 +36,13 @@ export class DownloadDataComponent implements OnInit {
 
   ngOnInit(): void {
     window.open(this.download_link, '_blank');
+  }
+
+  copyLink(): void {
+    this.clipboard.copy(this.retrieve_link);
+    this.snackBar.open('Link in die Zwischenablage kopiert!', 'Schliessen', {
+      duration: 2000,  // Notification lasts 2 seconds
+    });
   }
 
 }
