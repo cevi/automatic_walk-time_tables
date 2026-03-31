dc8c4ae8eb107e08758deab15473e127dce9b606
Cyrill Püntener <wp99cp@gmx.net>
2026-03-31 08:37:17 +0200

    feat: improvements to preview table

--- frontend/src/app/pages/download-data/download-data.component.ts
+++ frontend/src/app/pages/download-data/download-data.component.ts
@@ -31,7 +31,7 @@ export class DownloadDataComponent implements OnInit {
     // the data is available for 10 minutes
     const available_until = new Date();
     available_until.setMinutes(available_until.getMinutes() + 10);
-    this.available_until = available_until.toLocaleString();
+    this.available_until = available_until.toLocaleString('de-CH');
 
   }
 
